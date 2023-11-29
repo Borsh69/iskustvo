a8295b0cd214db5e29f48098960e17692db3f113
Borsh69 <uludintsevkot@gmail.com>
2023-11-29 12:27:07 +0300

    Timur cakez ti lox

--- base/forms.py
+++ base/forms.py
@@ -16,3 +16,19 @@ class RegistForm(forms.ModelForm):
     class Meta:
         model=User
         fields = ['name','tag', 'avatar', 'login', 'password'] 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-regist'}),
+            'tag': forms.TextInput(attrs={'class': 'form-regist'}),
+            'avatar': forms.FileInput(attrs={'class': 'form-regist'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-regist', type: 'password'}),
+            'login': forms.TextInput(attrs={'class': 'form-regist'})
+
+        }
+
+class AddArtWork(forms.Form):
+    face = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-regist'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-regist'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-regist'}))
+    uncompressed_img = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-regist'}), required=False)
+    object3d = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-regist'}), required=False)
+    tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-regist', 'placeholder':'example1, example2, example3'}), required=False)
